481e911268e94d21f4f589b6cc44f2542c6b246c
zoro_dev <zorodev.block@gmail.com>
2024-07-12 23:45:38 +0800

    no message

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import WebApp from '@twa-dev/sdk'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,17 +21,16 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          计数是 {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        {/* 在此处添加带有警告回调的按钮 */}
+      <div className="card">
+        <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
+            显示警告
+        </button>
+      </div>
     </>
   )
 }
-
 export default App
